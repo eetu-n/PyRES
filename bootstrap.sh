d4b1076a9eca47565039a53b2ddb65304428bc87
Gian Marco De Bortoli <74830976+GianMarcoDeBortoli@users.noreply.github.com>
2025-04-26 15:38:49 +0300

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Creating virtual environment 'pyres-env'..."
-python3 -m venv pyres-env
+python3.10 -m venv pyres-env
 
 echo "Activating virtual environment..."
 source pyres-env/bin/activate
@@ -10,7 +10,7 @@ echo "Upgrading pip..."
 pip install --upgrade pip setuptools wheel
 
 echo "Installing PyRES and dependencies..."
-pip install .
+pip install -r requirements.txt
 
 echo "Installation complete."
 echo "To activate the environment later, run:"
