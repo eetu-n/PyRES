73b4a9ad284e338f7443ad5c3f9eb66697ce2564
Gian Marco De Bortoli <74830976+GianMarcoDeBortoli@users.noreply.github.com>
2025-04-26 15:40:13 +0300

    Update bootstrap.bat

--- bootstrap.bat
+++ bootstrap.bat
@@ -4,14 +4,14 @@ echo Creating virtual environment 'pyres-env'...
 python -m venv pyres-env
 
 echo Activating virtual environment...
-call pyres-env\Scripts\activate.bat
+call pyres-env\Scripts\activate
 
 echo Upgrading pip...
-python -m pip install --upgrade pip setuptools wheel
+pip install --upgrade pip setuptools wheel
 
 echo Installing PyRES and dependencies...
-pip install .
+pip install -r requirements.txt
 
 echo Installation complete.
 echo To activate the environment later, run:
-echo call pyres-env\Scripts\activate.bat
+echo call pyres-env\Scripts\activate
